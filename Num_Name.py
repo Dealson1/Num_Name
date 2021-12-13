ec85f2bbe1be36155f9f5c1221833bea46ad7fff
Alexey <alexborn950@gmail.com>
2021-12-13 16:14:08 +0200

    Fix1

--- Num_Name.py
+++ Num_Name.py
@@ -1,98 +1,52 @@
 from module import *
 from tkinter import *
-dict = {"А": "1", "Б": "2", "В": "3", "Г": "4", "Д": "5", "Е": "6", "Ё": "7", "Ж": "8", "З": "9", "И": "1", "Й": "2", "К": "3", "Л": "4", "М": "5", "Н": "6", "О": "7", "П": "8", "Р": "9", "С": "1", "Т": "2", "У": "3", "Ф": "4", "Х": "5", "Ц": "6", "Ч": "7", "Ш": "8", "Щ": "9", "Ъ": "1", "Ы": "2", "Ь": "3", "Э": "4", "Ю": "5", "Я": "6"}
-letters_list = readDictKeys(dict)
-numb_list = readDictVal(dict)
-def readDictVal(dic: dict)->list:
-    """
-    Читает значения из словаря и добавляем их в список
-    :param dict dic: Название словаря
-    """
-    mas = []
-    for v in dic.values():
-        mas.append(v.strip(''))
-    return mas
-
-def readDictKeys(dic: dict)->list:
-    """
-    Читаем ключи из словаря и добавляем их в список
-    :param dict dic: Название словаря
-    """
-    mas = []
-    for v in dic.keys():
-        mas.append(v.strip(''))
-    return mas
-
-def getNameNumber(numb: list, lett: list, name: str):
-    """
-    Берем введеное имя и раскладываем его в список. Сравниваем буквы из имени с буквами и их значениями из готового списка. Считаем полученные цифры и раскладываем сумму, если нужно
-    :param list numb: Список с цифрами
-    :param list lett: Список с буквами
-    :param str name: Введенное имя
-    """
-    name_list = list(name)
-    print(name_list)
-    name_numbers = 0
-    n = 0
-    sum = 0
-    for i in name_list:
-        l = name_list[n]
-        index = lett.index(l)
-        numbers = numb[index]
-        sum += int(numbers)
-        n += 1
-
-    print(sum)
-    sum1 = 0
-    sum2 = 0
-    sum3 = 0
-    k = 0
-
-    if sum < 10:
-        text = (f"Число имени - {sum}")
-    elif sum >= 10:
-        sum1 = sum % 10
-        sum2 = sum // 10
-        sum3 = sum1 + sum2
-        if sum3 < 10:
-            text = (f"Число вашего имени - {sum3}")
-        elif sum3 >= 10:
-            a = sum3 // 10
-            b = sum3 % 10
-            k = a + b
-            text = (f"Число вашего имени - {k}")
-    return text
-
+dictrus = {"А": "1", "Б": "2", "В": "3", "Г": "4", "Д": "5", "Е": "6", "Ё": "7", "Ж": "8", "З": "9", "И": "1", "Й": "2", "К": "3", "Л": "4", "М": "5", "Н": "6", "О": "7", "П": "8", "Р": "9", "С": "1", "Т": "2", "У": "3", "Ф": "4", "Х": "5", "Ц": "6", "Ч": "7", "Ш": "8", "Щ": "9", "Ъ": "1", "Ы": "2", "Ь": "3", "Э": "4", "Ю": "5", "Я": "6"}
+dicteng = {"A": "1", "B": "2", "C": "3", "D": "4", "E": "5", "F": "6", "G": "7", "H": "8", "I": "9", "J": "1", "K": "2", "L": "3", "M": "4", "N": "5", "O": "6", "P": "7", "Q": "8", "R": "9", "S": "1", "T": "2", "U": "3", "V": "4", "W": "5", "X": "6", "Y": "7", "Z": "8"}
+letters_eng = Key(dicteng)
+numb_eng = Val(dicteng)
+letters_rus = Key(dictrus)
+numb_rus = Val(dictrus)
 
 def out(event):
-    while True:
-        try:
-            name = ent1.get()
-            verif = name.isalpha()
+    ''' Берет данные из ячеек, проверяет их и выводит их в функцию getNameNumber()
+    Выводит текст в lbl4
+    '''
+    try:
+        name = str(ent1.get())
+        verif = name.isalpha()
+    except ValueError:
+        text = 'Введите имя, состоящее из букв'
 
-        except ValueError:
-            print("Введите имя, состоящее из букв.")
-
-        if verif == True:
-            nameu = name.upper()
-            text = getNameNumber(numb_list, letters_list, nameu)
-            lbl3.configure(text = text)
+    if verif == True:
+        nameu = name.upper()
+        if re.search('[aA-zZ]', name):
+            text = getNameNumber(numb_eng, letters_eng, nameu)
+        elif re.search('[а-яА-ЯёЁ]', name):
+            text = num_name(numb_rus, letters_rus, nameu)
+        else:
+            text = 'Введите имя на русском или латинском'
+    else:
+        text = 'Введите имя, состоящее из букв'
+    lbl3.configure(text = text)
 
 window = Tk()
 window.title("Num - Name")
 window.geometry("600x300")
+window.configure(bg = "lightblue")
 
-lbl1 = Label(window, text = "Нумерология - Число имени", font = "Arial 15")
-lbl2 = Label(window, text = "Введите имя, состоящее из букв: ", font = "Arial 15")
-ent1 = Entry(bg = "lightblue", width = 10, font = "Arial 15")
-btn = Button(window, text = "Посчитать", font = "Arial 15", fg = "Black", bg = "lightgreen", width = 8, height = 1)
-lbl3 = Label(text = "", font = "Arial 15", bg = "lightyellow")
+lbl1 = Label(window, text = "НУМЕРОЛОГИЯ - ЧИСЛО ИМЕНИ", font = "Arial 15")
+lbl2 = Label(window, text = "Введите имя: ", font = "Arial 15")
+lbl3 = Label(window, text = "", font = "Arial 15")
 
-btn.bind("<Button-1>", out)
-ent1.bind("<Return>")
+ent1 = Entry(bg = "lightgray", width = 10, font = "Arial 15")
+
+btn = Button(window, text = "Посчитать", font = "Arial 15", width = 8, height = 1)
+
+btn.bind('<Button-1>', out)
+ent1.bind('<Return>')
 
 lbl1.pack()
-lbl2.pack()
+lbl2.pack(side = TOP)
 ent1.pack()
 btn.pack()
 lbl3.pack()
--- module.py
+++ module.py
@@ -1,6 +1,5 @@
-def readDictVal(dic: dict)->list:
+def Val(dic: dict)->list:
     """
-    Читает значения из словаря и добавляем их в список
     :param dict dic: Название словаря
     """
     mas = []
@@ -8,9 +7,8 @@ def readDictVal(dic: dict)->list:
         mas.append(v.strip(''))
     return mas
 
-def readDictKeys(dic: dict)->list:
+def Key(dic: dict)->list:
     """
-    Читаем ключи из словаря и добавляем их в список
     :param dict dic: Название словаря
     """
     mas = []
@@ -18,15 +16,14 @@ def readDictKeys(dic: dict)->list:
         mas.append(v.strip(''))
     return mas
 
-def getNameNumber(numb: list, lett: list, name: str):
+
+def num_name(numb: list, lett: list, name: str):
     """
-    Берем введеное имя и раскладываем его в список. Сравниваем буквы из имени с буквами и их значениями из готового списка. Считаем полученные цифры и раскладываем сумму, если нужно
     :param list numb: Список с цифрами
     :param list lett: Список с буквами
     :param str name: Введенное имя
     """
     name_list = list(name)
-    print(name_list)
     name_numbers = 0
     n = 0
     sum = 0
@@ -37,22 +34,22 @@ def getNameNumber(numb: list, lett: list, name: str):
         sum += int(numbers)
         n += 1
 
-    print(sum)
     sum1 = 0
     sum2 = 0
     sum3 = 0
     k = 0
 
     if sum < 10:
-        print(f"Число имени - {sum}")
+        text = f"Число имени - {sum}"
     elif sum >= 10:
         sum1 = sum % 10
         sum2 = sum // 10
         sum3 = sum1 + sum2
         if sum3 < 10:
-            print(f"Число вашего имени - {sum3}")
+            text = f"Число имени - {sum3}"
         elif sum3 >= 10:
             a = sum3 // 10
             b = sum3 % 10
             k = a + b
-            print(f"Число вашего имени - {k}")
+            text = f"Число имени - {k}"
+    return text
